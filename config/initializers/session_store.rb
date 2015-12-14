f0a16eef8efef6eeaa0962e723a7b332e6afff10
Chris Umbach <chrisumbach@gmail.com>
2015-12-14 09:09:39 -0800

    fixed database names

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-Rails.application.config.session_store :cookie_store, key: '_FridgerChef_session'
+Rails.application.config.session_store :cookie_store, key: '_FoodieFridge_session'
